b470e19661539cda0d46b6c18583e60026bf2369
Italo Rennan <italorennanls@gmail.com>
2020-11-09 23:54:21 -0300

    Calculadora com formatação

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,30 +4,29 @@ class Calculator extends React.Component {
     render() {
         return (
             <>
-                <span>{this.props.valueExhibited}</span>
+                <div class="display">{this.props.valueExhibited}</div>
                 <br/>
-                <button onClick={() => this.props.write('7')}>7</button>
-                <button onClick={() => this.props.write('8')}>8</button>
-                <button onClick={() => this.props.write('9')}>9</button>
+                <button class="number" onClick={() => this.props.write('7')}>7</button>
+                <button class="number" onClick={() => this.props.write('8')}>8</button>
+                <button class="number" onClick={() => this.props.write('9')}>9</button>
+                <button class="operator" onClick={() => this.props.write('+')}>+</button>
                 <br />
-                <button onClick={() => this.props.write('4')}>4</button>
-                <button onClick={() => this.props.write('5')}>5</button>
-                <button onClick={() => this.props.write('6')}>6</button>
+                <button class="number" onClick={() => this.props.write('4')}>4</button>
+                <button class="number" onClick={() => this.props.write('5')}>5</button>
+                <button class="number" onClick={() => this.props.write('6')}>6</button>
+                <button class="operator" onClick={() => this.props.write('-')}>-</button>
                 <br />
-                <button onClick={() => this.props.write('1')}>1</button>
-                <button onClick={() => this.props.write('2')}>2</button>
-                <button onClick={() => this.props.write('3')}>3</button>
+                <button class="number" onClick={() => this.props.write('1')}>1</button>
+                <button class="number" onClick={() => this.props.write('2')}>2</button>
+                <button class="number" onClick={() => this.props.write('3')}>3</button>
+                <button class="operator" onClick={() => this.props.write('*')}>*</button>
                 <br />
-                <button onClick={() => this.props.clean()}>C</button>
-                <button onClick={() => this.props.write('0')}>0</button>
-                <button onClick={() => this.props.write('.')}>.</button>
+                <button class="number" onClick={() => this.props.clean()}>C</button>
+                <button class="number" onClick={() => this.props.write('0')}>0</button>
+                <button class="number" onClick={() => this.props.write('.')}>.</button>
+                <button class="operator" onClick={() => this.props.write('/')}>/</button>
                 <br />
-                <button onClick={() => this.props.write('+')}>+</button>
-                <button onClick={() => this.props.write('-')}>-</button>
-                <button onClick={() => this.props.write('*')}>*</button>
-                <button onClick={() => this.props.write('/')}>/</button>
-                <br />
-                <button onClick={() => this.props.evaluate()}>=</button>
+                <button class="eval" onClick={() => this.props.evaluate()}>=</button>
             </>
         );
     }
